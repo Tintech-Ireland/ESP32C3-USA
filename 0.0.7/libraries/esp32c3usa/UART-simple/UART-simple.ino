851ba45690bafbda61778e5e7ffc22752107ed0b
Pawad Imhotep <x.y@z>
2025-05-11 13:12:47 +0100

    Changed to version 7 to force update of arduino cache

---
+++ 0.0.7/libraries/esp32c3usa/UART-simple/UART-simple.ino
@@ -0,0 +1,69 @@
+#include <HardwareSerial.h>
+
+// the setup function runs once when you press reset or power the board
+HardwareSerial SerialPort(0); // use UART0
+#define USBSerial Serial
+
+#define UART_RX_PIN_RS232  4
+#define UART_TX_PIN_RS232  5
+
+#define UART_RX_PIN_RS485  8
+#define UART_TX_PIN_RS485  9
+
+// Change this depending on which port you want to test
+
+// To test RS232
+// #define UART_RX_PIN  UART_RX_PIN_RS232
+// #define UART_TX_PIN  UART_TX_PIN_RS232
+
+// To test RS485
+// #define UART_RX_PIN  UART_RX_PIN_RS485
+// #define UART_TX_PIN  UART_TX_PIN_RS485
+
+#define UART_RX_PIN  UART_RX_PIN_RS232
+#define UART_TX_PIN  UART_TX_PIN_RS232
+
+// int my_vprintf(const char *format, va_list args) {
+//     char *log_buffer;
+//     size_t len = vasprintf(&log_buffer, format, args);
+//     SerialPort.println(log_buffer);
+//     return len;
+// }
+
+// TWO serial ports - one goes out on UART, the Other on the USB
+// "USB CDC on boot" must be set to "disabled" to activate USBSerial
+void setup() {
+  SerialPort.begin(115200, SERIAL_8N1, UART_RX_PIN, UART_TX_PIN);
+  USBSerial.begin(115200) ;
+  SerialPort.setDebugOutput(false); // stops debug coming out of the serial port
+  USBSerial.setDebugOutput(true);   // debug comes out of the USB serial port - set to false if you want to use
+                                    // it for something else
+  delay(1000);
+
+  esp_log_level_set("*", ESP_LOG_ERROR);
+
+  // Build settings have NO effect on these
+  log_e("log_e %s", "a string error");
+  log_w("log_w %s", "a string warning");
+  log_i("log_i %s", "a string info");
+  log_d("log_d %s", "a string debug");
+
+  SerialPort.println("Start Serial");
+  ESP_LOGI("blink", "Start Log blink");
+  USBSerial.println("Start USB");
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(10, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(2000);                      // wait for a second
+  digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
+  delay(2000);                      // wait for a second
+  SerialPort.println("Blinks serial");
+  USBSerial.println("Blinks USB");
+  log_d("log_d %s", "Loop string debug");
+  ESP_LOGI("blink", "Loop Log blink");
+}
